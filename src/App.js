6a3cf8cec3bf342d7219098eb0a262f08217b21c
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-10-11 19:59:43 -0400

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 import './index.css'
 
 const App = () =>{
-    return(<></>)
+    return(<>
+    <Navbar />
+    
+    
+    </>)
 }
 export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
+  themes:['night'],
+  plugins: [
+      
+      require("daisyui")
+  ],
+};
