7ae159e613b3e1b9b1092fa125005c8541c6cd9d
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-10-27 18:57:22 -0400

    s

--- src/Components/WeatherDisplay.js
+++ src/Components/WeatherDisplay.js
@@ -10,14 +10,14 @@ const WeatherDisplay = () =>{
                 <div className="flex-col">
                 <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
                 <div className="divider"></div> 
-                <h2 className="text-5xl font-bold text-center px-3">20 C</h2>
+                <h2 className="text-5xl font-bold text-center px-3">-</h2>
                 </div>
                 
                 <div className="bg-neutral text-center p-4 rounded-lg">
-                    <h1 className="text-5xl font-bold">Location</h1>
+                    <h1 className="text-5xl font-bold">Search for location</h1>
                     <div className="divider"></div> 
                     <WeatherInfo />
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">-</p>
 
                 </div>
             </div>
